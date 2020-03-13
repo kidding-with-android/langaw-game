6be3a6bb0c8564c1850b96a8c373ef32449c4803
Rafael Neves <rafael@giver.com.br>
2020-03-13 14:12:02 -0300

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flame/util.dart';
+
+import 'package:langaw/langaw-game.dart';
+
+void main() async {
+	WidgetsFlutterBinding.ensureInitialized();
+
+	Util flameUtil = Util();
+	await flameUtil.fullScreen();
+	await flameUtil.setOrientation(DeviceOrientation.portraitUp);
+
+	LangawGame game = LangawGame();
+	runApp(game.widget);
+
+	TapGestureRecognizer tapper = TapGestureRecognizer();
+	tapper.onTapDown = game.onTapDown;
+	flameUtil.addGestureRecognizer(tapper);
+}
