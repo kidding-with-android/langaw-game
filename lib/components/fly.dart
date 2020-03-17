475369a32c9694eb92d104040021545259bb4249
Rafael Neves <rafael@giver.com.br>
2020-03-17 14:05:22 -0300

    Flies animation

--- lib/components/fly.dart
+++ lib/components/fly.dart
@@ -1,22 +1,30 @@
 import 'dart:ui';
+import 'package:flame/sprite.dart';
 
 import 'package:langaw/langaw-game.dart';
 
 class Fly {
 	final LangawGame game;
+	List<Sprite> flyingSprite;
+	Sprite deadSprite;
+	double flyingSpriteIndex = 0;
 	Rect flyRect;
-	Paint flyPaint;
 	bool isDead = false;
 	bool isOffScreen = false;
+	Offset targetLocation;
 
-	Fly(this.game, double x, double y) {
-		flyRect = Rect.fromLTWH(x, y, game.tileSize, game.tileSize);
-		flyPaint = Paint();
-		flyPaint.color = Color(0xff6ab04c);
+	Fly(this.game) {
+		setTargetLocation();
 	}
 
+	double get speed => game.tileSize * 3;
+
   void render(Canvas c) {
-		c.drawRect(flyRect, flyPaint);
+		if (isDead) {
+			deadSprite.renderRect(c, flyRect.inflate(2));
+		} else {
+			flyingSprite[flyingSpriteIndex.toInt()].renderRect(c, flyRect.inflate(2));
+		}
 	}
 
   void update(double t) {
@@ -26,12 +34,34 @@ class Fly {
 			if (flyRect.top > game.screenSize.height) {
 				isOffScreen = true;
 			}
+		} else {
+			// flap de wings
+			flyingSpriteIndex += 30 * t;
+			if (flyingSpriteIndex >= 2) {
+				flyingSpriteIndex -= 2;
+			}
+
+			// move the fly
+			double stepDistance = speed * t;
+			Offset toTarget = targetLocation - Offset(flyRect.left, flyRect.top);
+			if (stepDistance < toTarget.distance) {
+				Offset stepToTarget = Offset.fromDirection(toTarget.direction, stepDistance);
+				flyRect = flyRect.shift(stepToTarget);
+			} else {
+				flyRect = flyRect.shift(toTarget);
+				setTargetLocation();
+			}
 		}
+	}
+
+	void setTargetLocation() {
+		double x = game.rnd.nextDouble() * (game.screenSize.width - (game.tileSize * 2.025));
+		double y = game.rnd.nextDouble() * (game.screenSize.height - (game.tileSize * 2.025));
+		targetLocation = Offset(x, y);
 	}
 
 	void onTapDown() {
 		isDead = true;
-		flyPaint.color = Color(0xffff4757);
 		game.spawnFly();
 	}
 }
--- lib/langaw-game.dart
+++ lib/langaw-game.dart
@@ -6,6 +6,11 @@ import 'package:flame/game.dart';
 
 import 'package:langaw/components/backyard.dart';
 import 'package:langaw/components/fly.dart';
+import 'package:langaw/components/house-fly.dart';
+import 'package:langaw/components/agile-fly.dart';
+import 'package:langaw/components/drooler-fly.dart';
+import 'package:langaw/components/hungry-fly.dart';
+import 'package:langaw/components/macho-fly.dart';
 
 class LangawGame extends Game {
   Size screenSize;
@@ -52,8 +57,24 @@ class LangawGame extends Game {
 	}
 
 	void spawnFly() {
-		double x = rnd.nextDouble() * (screenSize.width - tileSize);
-		double y = rnd.nextDouble() * (screenSize.height - tileSize);
-		flies.add(Fly(this, x, y));
+		double x = rnd.nextDouble() * (screenSize.width - (tileSize * 2.025));
+		double y = rnd.nextDouble() * (screenSize.height - (tileSize * 2.025));
+		switch (rnd.nextInt(5)) {
+			case 0:
+				flies.add(HouseFly(this, x, y));
+				break;
+			case 1:
+				flies.add(DroolerFly(this, x, y));
+				break;
+			case 2:
+				flies.add(AgileFly(this, x, y));
+				break;
+			case 3:
+				flies.add(MachoFly(this, x, y));
+				break;
+			case 4:
+				flies.add(HungryFly(this, x, y));
+				break;
+		}
 	}
 }
